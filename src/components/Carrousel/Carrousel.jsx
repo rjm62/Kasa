9e9b8790430e6c1aa95388c593446615359f1769
rjm <jeanmichelrochoy62@gmail.com>
2023-11-14 21:30:58 +0100

    modif responsive

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -35,7 +35,7 @@ function Carrousel({image}) {
                     <i onClick={next} className="fa-solid fa-chevron-right" ></i>   
                 </div>
             ):("")}       
-            <p>{selectionNumber}</p>
+            <p> {selectionNumber} </p>
             <img src={image[selection]} alt ="appartement"></img>           
         </div>    
     )
